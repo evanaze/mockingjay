5eee44c50c9dd830aafc2908fc892f60e470932a
Evan Azevedo <evan@azevedo.com>
2022-06-23 08:32:18 -0700

    working on data processing

---
+++ placebot/finetune.py
@@ -0,0 +1,10 @@
+"""Fine-tunes the model based on the dataset of tweets."""
+
+
+class FineTune:
+    
+    def __init__(self, model_path: str) -> None:
+        """Fine tunes the model.
+
+        :param model_path: The actual path to the model to fine-tune
+        """
---
+++ placebot/generate.py
@@ -0,0 +1,1 @@
+"""Generates synthetic tweets."""
